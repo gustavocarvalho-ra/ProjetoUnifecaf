aaec39161925e0479903a327ed26cef0736aa1f3
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-27 23:45:05 -0300

    feat: test controller

--- src/controllers/FilmeController.js
+++ src/controllers/FilmeController.js
@@ -19,7 +19,26 @@ class FilmeController {
         return res.status(404).json({ error: "Filme não encontrado" });
       }
       
-      res.status(200).json(filme)
+      res.status(200).json(filme);
+    } catch (error) {
+      res.status(500).json({ error: "Erro ao buscar filme" });
+    }
+  }
+
+  static async filtrar(req, res) {
+    try {
+      const { nome } = req.query;
+
+      if (!nome) {
+        return res.status(400).json({ error: 'Parâmetro "nome" é obrigatório' });
+      }
+
+      const filmes = await Filme.search(nome);
+      res.status(200).json(filmes);
+    } catch (error) {
+      return res.status(500).json({ error: "Erro ao filtrar filmes" });
     }
   }
 }
+
+module.exports = FilmeController;
