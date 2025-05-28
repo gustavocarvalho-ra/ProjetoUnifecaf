690d77543011588779ef6bea74ef07d11f1984c9
GustavoCR021 <gustavoc1922@gmail.com>
2025-05-28 00:04:23 -0300

    feat: add coments

--- src/app.js
+++ src/app.js
@@ -5,15 +5,19 @@ require("dotenv").config();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
+//Rotas
 app.use("/", filmeRoutes);
 
+//Rotas padrão
 app.get("/", (req, res) => {
   res.send("API FECAF Flix - Controle de Filmes");
 });
 
+//Tratamento de erros 404
 app.use((req, res) => {
   res.status(404).json({ error: "Endpoint não encontrado" });
 });
